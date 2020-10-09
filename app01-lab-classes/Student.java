29d3e6dbee3e66a1251778beeacd5a7885e5c3bc
drjellybaby <rognen@live.co.uk>
2020-10-09 20:18:25 +0100

    Formatting and recode
    recoded to better fit requirements, and formatted for easy understanding

--- app01-lab-classes/Student.java
+++ app01-lab-classes/Student.java
@@ -100,7 +100,6 @@ public class Student
     public void print()
     {
         System.out.println(name + ", student ID: " + id + ", credits: " + credits);
-        
-
+        course.print();
     }
 }
